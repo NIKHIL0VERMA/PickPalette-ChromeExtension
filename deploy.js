29e1e85e53e822f8534805472e55cd690c22dd4c
NIKHIL0VERMA <nikhil2003verma@gmail.com>
2024-05-26 22:23:30 +0530

    changes done

--- deploy.js
+++ deploy.js
@@ -4,17 +4,17 @@ const jsonfile = require('jsonfile');
 const path = require('path');
 const ChromeWebstore = require('chrome-webstore-manager');
 
-const itemId = "EXTENSION_ID"; // TODO Change this after publishing the extension on web store
-var buildLocation = path.join(__dirname, "Web2palette");
+const itemId = process.env.EXTENSION_ID; 
+const buildLocation = path.join(__dirname, "Web2palette");
 
 // read manifest file
-var manifest = jsonfile.readFileSync(path.join(buildLocation, "manifest.json"));
+const manifest = jsonfile.readFileSync(path.join(buildLocation, "manifest.json"));
 function getNewVersion() {
-    var ver = parseInt(manifest.version.split('.')[0]);
+    const ver = parseInt(manifest.version.split('.')[0]);
     ver++;
     return `${ver}.0.0`;
 }
-var version = getNewVersion();
+const version = getNewVersion();
 // replace version
 manifest.version = version;
 // save manifest file
@@ -24,10 +24,8 @@ jsonfile.writeFileSync(path.join(buildLocation, "manifest.json"), manifest);
 zipper.sync.zip(buildLocation).compress().save(path.join(buildLocation, "build.zip"));
 const fileBin = fs.readFileSync(path.join(buildLocation, "build.zip"));
 
-// TODO change the client id and clientSecret
-const chromeWebstore = new ChromeWebstore("<CLIENT_ID>", "<CLIENT_SECRET>");
-// Get new token with refresh_token
-chromeWebstore.getRefreshToken("<REFRESH_TOKEN>").then(function (data) {
+const chromeWebstore = new ChromeWebstore(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+chromeWebstore.getRefreshToken(process.env.REFRESH_TOKEN).then(function (data) {
     const json = JSON.parse(data)
     const newToken = json.access_token
     chromeWebstore.updateItem(newToken, fileBin, itemId).then((data) => {
